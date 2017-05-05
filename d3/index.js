9d5447129515773a602c6016d9688c3014856534
David Castillo <davcs86@gmail.com>
2017-05-05 14:01:57 -0500

    ...

--- d3/index.js
+++ d3/index.js
@@ -1,10 +1,33 @@
 export {
-  event,
   mouse,
   select,
-  selection,
   selectAll
 } from 'd3-selection';
+
+import {selection as Selection, event} from 'd3-selection';
+
+//prototype. delegated events
+Selection.prototype.delegate = function(evt, targetselector, handler) {
+  var self = this;
+  return this.on(evt, function() {
+    var eventTarget = event.target,
+        target = self.selectAll(targetselector);
+    target.each(function(){
+      //only perform event handler if the eventTarget and intendedTarget match
+      if (eventTarget === this) {
+        handler.call(eventTarget, eventTarget.__data__);
+      } else if (eventTarget.parentNode === this) {
+        handler.call(eventTarget.parentNode, eventTarget.parentNode.__data__);
+      }
+    });
+  });
+};
+
+export {
+  Selection as selection,
+  event
+};
+
 
 export {
   zoom,
